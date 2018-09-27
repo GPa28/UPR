7101cb5f4f8b940ff301e041b8e302d21fda58f7
ThunderBadger <BadgerBatIndustries@gmail.com>
2018-09-26 20:07:28 -0600

    Add top ten model , controller, db table, and view

---
+++ UltimatePowerRankings/UltimatePowerRankings/Data/Migrations/20180927013611_AddTopTenListTable.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace UltimatePowerRankings.Data.Migrations
+{
+    public partial class AddTopTenListTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TopTenLists",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Title = table.Column<string>(nullable: true),
+                    One = table.Column<string>(nullable: true),
+                    Two = table.Column<string>(nullable: true),
+                    Three = table.Column<string>(nullable: true),
+                    Four = table.Column<string>(nullable: true),
+                    Five = table.Column<string>(nullable: true),
+                    Six = table.Column<string>(nullable: true),
+                    Seven = table.Column<string>(nullable: true),
+                    Eight = table.Column<string>(nullable: true),
+                    Nine = table.Column<string>(nullable: true),
+                    Ten = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TopTenLists", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TopTenLists");
+        }
+    }
+}
